f9d7f2a827b98476e30668be00375cb4236320c6
Caden A <caruta@umd.edu>
2026-02-06 18:51:08 -0500

    Added new route for HTML page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World from Caden!'
+def hello_world(): #put application's code here
+    return "Hello World from Caden! I am adding my first code change."
+
+@app.route('/hello')
+def hello():
+    return render_template("hello.html")
 
 
 if __name__ == '__main__':
